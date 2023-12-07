d465227072941bd7027c74fbc305fc0766006040
waleed2000x <waleedahmed2000x@gmail.com>
2023-12-07 13:03:32 +0500

    CORS!~

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const router = require("./routes/routes");
 const app = express();
 
 dotenv.config({ path: "./config.env" });
+const corsOptions = {
+  origin: ["https://book-haven-hub.vercel.app", "http://localhost:5173"],
+  credentials: false,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/books", router);
