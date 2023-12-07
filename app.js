fcb9fea479e43303becdc77e44f2283e9e2bcdba
waleed2000x <waleedahmed2000x@gmail.com>
2023-12-07 12:43:13 +0500

    root directory for deploy

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express();
 
 dotenv.config({ path: "./config.env" });
 const corsOptions = {
-  origin: ["https://book-haven-hub.vercel.app", "http://localhost:5173"],
+  //   origin: ["https://book-haven-hub.vercel.app", "http://localhost:5173"],
+  origin: ["*"],
   credentials: false, // if you are using cookies or authentication headers
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 };
